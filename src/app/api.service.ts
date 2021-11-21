97a7509464fb5307f95938314d98df8cd98443d3
johnloydVramos <ramos.johnloyd2@gmail.com>
2021-11-21 21:47:53 +0800

    commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  headers: HttpHeaders;
+
+  constructor(public http: HttpClient){
+    this.headers = new HttpHeaders();
+    this.headers.append('Accept', 'application/json');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Access-Control-Allow-Origin', '*');
+
+
+  }
+
+  addStudent(data){
+    return this.http.post('http://localhost/Ionic_CRUD_APP/backend/create.php', data);
+
+  }
+}
+
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  student_no: any;
+  student_name: any;
+  student_add: any;
+  student_course: any;
+  student_year: any;
 
-  constructor() {}
+
+  constructor(public _apiService: ApiService) {}
+
+  addStudent(){
+  let data = {
+    student_no: this.student_no,
+    student_name: this.student_name,
+    student_add: this.student_add,
+    student_course: this.student_course,
+    student_year: this.student_year,
+
+  }
+
+  this._apiService.addStudent(data).subscribe((res:any) =>{
+    console.log("Success ==", res);
+  },(error:any) =>{
+    console.log("Error==", error);
+  })
+  }
 
 }
